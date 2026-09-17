d4f94c0970fe5546cd0853b4ed15da7cc7f0bfff
Bauszz <gmezoe@gmail.com>
2026-09-17 08:36:04 +0200

    files

---
+++ index.js
@@ -0,0 +1,50 @@
+const filmek = [
+{
+    "title": "Ratatouille",
+    "year": 2007,
+    "genre": "Animation",
+    "rating": 5
+  },
+  {
+    "title": "Kung Fu Panda",
+    "year": 2008,
+    "genre": "Animation",
+    "rating": 4
+  },
+  {
+    "title": "Up",
+    "year": 2009,
+    "genre": "Animation",
+    "rating": 4
+  },
+  {
+    "title": "Toy Story 3",
+    "year": 2010,
+    "genre": "Animation",
+    "rating": 1
+  },
+  {
+    "title": "Frozen",
+    "year": 2013,
+    "genre": "Animation",
+    "rating": 4
+  },
+  {
+    "title": "Inside Out",
+    "year": 2015,
+    "genre": "Animation",
+    "rating": 5
+  },
+  {
+    "title": "Zootopia",
+    "year": 2016,
+    "genre": "Animation",
+    "rating": 4
+  },
+  {
+    "title": "Coco",
+    "year": 2017,
+    "genre": "Animation",
+    "rating": 4
+  }
+];
